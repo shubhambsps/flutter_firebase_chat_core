d0e1310c2c4c4902b8443150d8079de5f1c72967
Shubham Bichkar <shubhamb.softprodigy@gmail.com>
2021-10-11 14:30:26 +0530

    FirebaseChatCore instanceFor added

--- lib/src/firebase_chat_core.dart
+++ lib/src/firebase_chat_core.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'util.dart';
@@ -20,6 +21,14 @@ class FirebaseChatCore {
   /// Singleton instance
   static final FirebaseChatCore instance =
       FirebaseChatCore._privateConstructor();
+
+  // ignore: sort_constructors_first
+  FirebaseChatCore.instanceFor({@required FirebaseApp app}){
+    // ignore: prefer_asserts_in_initializer_lists
+    assert(app != null);
+
+    firebaseUser = FirebaseAuth.instanceFor(app: app).currentUser;
+  }
 
   /// Creates a chat group room with [users]. Creator is automatically
   /// added to the group. [name] is required and will be used as
